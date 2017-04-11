815539a8c7c32790f550e4f1a3dcbf94eb23711f
eightfeet <bajiao000000@163.com>
2017-04-11 11:28:50 +0800

    Update App.js
    router fix

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,7 @@ export default class App extends Component {
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
 					<About path="/about" />
+					<Home default />
 				</Router>
 			</div>
 		);
